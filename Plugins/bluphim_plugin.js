ef6272b9edb8b9cac2ef6e8a278c7ebb46c23d91
namanhauto <vutuannam1995@gmail.com>
2026-03-19 01:05:22 +0700

    Update bluphim_plugin.js

--- Plugins/bluphim_plugin.js
+++ Plugins/bluphim_plugin.js
@@ -8,8 +8,7 @@ function getManifest() {
         "version": "1.0.9",          
         "baseUrl": "https://bluphim.me",
         "iconUrl": "https://bluphim.me/favicon.ico", 
-        "isEnabled": true,
-        "isAdult": false,            
+        "isEnabled": true,         
         "type": "MOVIE",
         "layoutType": "VERTICAL"
     });
